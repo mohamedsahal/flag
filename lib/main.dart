8e0ded0ca7f86c6a4421ad2658b030a31ebc6e9c
mohamedsahal <mohaansahal@gmail.com>
2022-03-26 17:18:27 +0300

    flag

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Image(
+            image: AssetImage('images/star.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
